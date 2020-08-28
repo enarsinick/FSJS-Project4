5f14f629f6b6dd23be88b3875e7fa809d9174119
Nick Cave <nxdmedia@googlemail.com>
2020-08-28 18:08:16 +0100

    Began building core functionality for game

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,33 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+     constructor() {
+         this.missed = 0;
+         this.phrases = this.createPhrases();
+         this.activePhrase = null;
+     }
+
+     createPhrases() {
+         let phraseArray = [];
+         phraseArray.push(new Phrase('thats the way the cookie crumbles'));
+         phraseArray.push(new Phrase('go back to the drawing board'));
+         phraseArray.push(new Phrase('call it a day'));
+         phraseArray.push(new Phrase('beat around the bush'))
+         phraseArray.push(new Phrase('its not rocket science'));;
+         return phraseArray;
+     }
+
+     getRandomPhrase() {
+         let randomNum = Math.floor((Math.random() * this.phrases.length));
+         return this.phrases[randomNum];
+     }
+
+     startGame() {
+        document.getElementById('overlay').style.display = 'none';
+        let randomPhrase = this.getRandomPhrase();
+        randomPhrase.addPhraseToDisplay();
+        this.activePhrase = randomPhrase;
+     }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,24 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+ class Phrase {
+     constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+     }
+
+     addPhraseToDisplay() {
+         let phraseAsArray = this.phrase.split('');
+         const phraseUL = document.querySelector('ul');
+         for (let i = 0; i < phraseAsArray.length; i++) {
+            let li = document.createElement('li');
+            li.textContent = phraseAsArray[i];
+            phraseUL.appendChild(li);
+            if (li.textContent !== ' ') {
+                li.classList.add('letter');
+            } else {
+                li.classList.add('space');
+            }
+        }
+     }
+ }
